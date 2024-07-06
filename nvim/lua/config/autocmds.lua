79e63d69e57892549d890bccdb6e019f0a360a6a
AayushBahukhandi <aayushpotter555@gmail.com>
2024-07-06 20:37:58 +0530

    new neovim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
+vim.keymap.set("n", "<leader>ta", vim.cmd.ASToggle, { desc = "Toggle ASToggle" })
